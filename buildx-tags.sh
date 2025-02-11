99e1f8262c81ce32e2b85f622fb5c8a31643d1e1
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-02-10 23:41:16 -0700

    [buildx-tags.sh] Parameterise org, docker args, and images

--- buildx-tags.sh
+++ buildx-tags.sh
@@ -11,26 +11,31 @@ set -o pipefail
 
 printf 'Start BUILDX\n'
 
+export ORG="${ORG:-devdotnetorg}"
+export DOCKER_ARGS="${DOCKER_ARGS:---push}"
+if [ -z ${IMAGES+x} ]; then
+  export IMAGES='ubuntu:16.04 ubuntu:18.04 ubuntu:20.04 ubuntu:22.04 ubuntu:23.10 ubuntu:24.04 debian:10 debian:11 debian:12'
+fi
+
 #:ubuntu 16.04, 18.04, 20.04, 22.04, 23.10, 24.04
 #:debian 10, 11, 12
-for IMAGE_VERSION in ubuntu:16.04 ubuntu:18.04 ubuntu:20.04 ubuntu:22.04 ubuntu:23.10 ubuntu:24.04 debian:10 debian:11 debian:12
+for IMAGE_VERSION in ${IMAGES}
 do
   #
   IMAGE_VERSION_2=$(printf '%s' "$IMAGE_VERSION" | tr : -)
   declare IMAGE_VERSION_2
   # build
   printf 'BUILD image: %s\n' "${IMAGE_VERSION}"
-  docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION="${IMAGE_VERSION}" -t devdotnetorg/openssh-server:"${IMAGE_VERSION_2}" . --push
-  echo docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION="${IMAGE_VERSION}" -t devdotnetorg/openssh-server:"${IMAGE_VERSION_2}" . --push
+  docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION="${IMAGE_VERSION}" -t "${ORG}"/openssh-server:"${IMAGE_VERSION_2}" . "${DOCKER_ARGS}"
   #
 done
 
 #latest ubuntu:24.04
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=ubuntu:24.04 -t devdotnetorg/openssh-server:ubuntu . --push
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=ubuntu:24.04 -t "${ORG}"/openssh-server:ubuntu . "${DOCKER_ARGS}"
 
 
 #latest debian:12
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=debian:12 -t devdotnetorg/openssh-server:debian . --push
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=debian:12 -t "${ORG}"/openssh-server:debian . "${DOCKER_ARGS}"
 
 #:alpine 3.16, 3.17, 3.18, 3.19, 3.20
 for IMAGE_VERSION in alpine:3.16 alpine:3.17 alpine:3.18 alpine:3.19 alpine:3.20
@@ -40,22 +45,22 @@ do
   declare IMAGE_VERSION_2
   # build
   printf 'BUILD image: %s\n' "${IMAGE_VERSION}"
-  docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f alpine.Dockerfile --build-arg IMAGE_VERSION="${IMAGE_VERSION}" -t devdotnetorg/openssh-server:"${IMAGE_VERSION_2}" . --push
+  docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f alpine.Dockerfile --build-arg IMAGE_VERSION="${IMAGE_VERSION}" -t "${ORG}"/openssh-server:"${IMAGE_VERSION_2}" . "${DOCKER_ARGS}"
   #
 done
 
 #latest alpine:3.20
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f alpine.Dockerfile --build-arg IMAGE_VERSION=alpine:3.20 -t devdotnetorg/openssh-server:alpine . --push
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f alpine.Dockerfile --build-arg IMAGE_VERSION=alpine:3.20 -t "${ORG}"/openssh-server:alpine . "${DOCKER_ARGS}"
 
 # RISC-V (riscv64)
 #:ubuntu-riscv64
-docker buildx build --platform linux/riscv64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=riscv64/ubuntu:22.04 -t devdotnetorg/openssh-server:ubuntu-riscv64 . --push
+docker buildx build --platform linux/riscv64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=riscv64/ubuntu:22.04 -t "${ORG}"/openssh-server:ubuntu-riscv64 . "${DOCKER_ARGS}"
 #:debian-riscv64
-docker buildx build --platform linux/riscv64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=riscv64/debian:sid -t devdotnetorg/openssh-server:debian-riscv64 . --push
+docker buildx build --platform linux/riscv64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=riscv64/debian:sid -t "${ORG}"/openssh-server:debian-riscv64 . "${DOCKER_ARGS}"
 #:alpine-riscv64
-docker buildx build --platform linux/riscv64 -f alpine.Dockerfile --build-arg IMAGE_VERSION=riscv64/alpine:edge -t devdotnetorg/openssh-server:alpine-riscv64 . --push
+docker buildx build --platform linux/riscv64 -f alpine.Dockerfile --build-arg IMAGE_VERSION=riscv64/alpine:edge -t "${ORG}"/openssh-server:alpine-riscv64 . "${DOCKER_ARGS}"
 
 #:latest ubuntu:24.04
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=ubuntu:24.04 -t devdotnetorg/openssh-server:latest . --push
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -f ubuntu.Dockerfile --build-arg IMAGE_VERSION=ubuntu:24.04 -t "${ORG}"/openssh-server:latest . "${DOCKER_ARGS}"
 
 printf 'BUILDX END\n'
